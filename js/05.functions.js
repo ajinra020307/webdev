caa34c9be4dda726913df5342ddd8e8cd9a75a4c
Ajin R <ajinra020307@gmail.com>
2022-01-22 17:31:04 +0530

    Update

--- js/05.functions.js
+++ js/05.functions.js
@@ -6,3 +6,41 @@ function add(a, b) {
 }
 
 add(4, 5)
+
+// return
+
+
+function add2(a, b) {
+
+    return a + b
+
+
+}
+
+let sum = add2(45, 454)
+
+
+// callback functions
+
+function one(callback) {
+
+    console.log('one')
+    callback()
+
+}
+
+function two() {
+
+    console.log('two')
+
+}
+
+one(two)
+
+// different way of writign functions
+
+let three = function(){
+
+
+
+}
